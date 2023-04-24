93400866ad33ee99a23ac0238280b1ce98228bd5
McCarthy <mccarthymutebi10@gmail.com>
2023-04-24 11:23:30 +0300

    testing out scrollIntoView using useRef or using the DOM directly

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 
 
-const About = () => {
+const About = ({ myWorkRef }) => {
   return (
-    <div id='about'>
+    <div id='about' ref={myWorkRef}>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam possimus consequuntur facilis odit nemo, expedita ratione placeat cupiditate, perferendis, reprehenderit totam! Repellat ratione corrupti numquam quia commodi natus ipsa!
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faM } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const scrollTo = () => {
+    const targetElement = document.getElementById('about')
+    targetElement.scrollIntoView({ behavior:'smooth'})
+  }
   return (
     <header>
       <div>
@@ -11,7 +15,7 @@ const Header = () => {
           <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/about'>About</Link></li>
-            <li><Link to='/work'>My Work</Link></li>
+            <li><Link to='/#about' onClick={scrollTo}>My Work</Link></li>
             <li><Link to='/contact'>Contact Me</Link></li>
           </ul>
         </nav>
