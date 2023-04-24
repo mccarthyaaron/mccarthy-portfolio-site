542a0ac9e36259a6bde316cd47939c1999f12b2a
McCarthy <mccarthymutebi10@gmail.com>
2023-04-24 12:17:06 +0300

    commit before diving branches into the SPA versus multi-page Application

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@
 
 const About = ({ myWorkRef }) => {
   return (
-    <div id='about' ref={myWorkRef}>
+    <div id='about'ref={myWorkRef}>
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquam possimus consequuntur facilis odit nemo, expedita ratione placeat cupiditate, perferendis, reprehenderit totam! Repellat ratione corrupti numquam quia commodi natus ipsa!
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,9 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faM } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () => {
-  const scrollTo = () => {
-    const targetElement = document.getElementById('about')
-    targetElement.scrollIntoView({ behavior:'smooth'})
+const Header = ({ myWorkRef }) => {
+  const scrollToMyWork = () => {
+    myWorkRef.current.scrollIntoView({ behavior:'smooth'})
   }
   return (
     <header>
@@ -15,7 +14,7 @@ const Header = () => {
           <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/about'>About</Link></li>
-            <li><Link to='/#about' onClick={scrollTo}>My Work</Link></li>
+            <li><Link to='/#work' onClick={scrollToMyWork}>My Work</Link></li>
             <li><Link to='/contact'>Contact Me</Link></li>
           </ul>
         </nav>
