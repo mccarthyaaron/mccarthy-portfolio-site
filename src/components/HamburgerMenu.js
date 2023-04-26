c930911d4456c8985cf874256f5d390c9afc2424
McCarthy <mccarthymutebi10@gmail.com>
2023-04-26 13:26:19 +0300

    final build

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 
 const HamburgerMenu = () => {
     const [showMenu, setShowMenu] = useState(false);
